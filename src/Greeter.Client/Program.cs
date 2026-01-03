19f59d08a6f3459353ac298bd0ad5e805d59425f
Renato Golia <renato@golia.tech>
2026-01-04 00:47:39 +0100

    Added console client

---
+++ src/Greeter.Client/Program.cs
@@ -0,0 +1,11 @@
+﻿using Greeter;
+
+using Grpc.Net.Client;
+using static Greeter.Greeter;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Renato" });
+
+Console.WriteLine("Greeting: " + reply.Message);
